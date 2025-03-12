c0532f3b6c068fe2d3fd2047fa95d8a21932ef8c
Eddie Hillenbrand <haltingproblems0@gmail.com>
2025-03-12 14:17:02 -0500

    Update Release configuration for archive builds
    This improves the build speed and properly installs the built
    frameworks into the archives.

--- Scripts/make/xcframework.mk
+++ Scripts/make/xcframework.mk
@@ -12,8 +12,6 @@ PLATFORM_iOS=iOS
 PLATFORM_iOS_Simulator='iOS Simulator'
 PLATFORM_Mac_Catalyst=macOS
 
-#VARIANT_iOS=
-#VARIANT_iOS_Simulator=
 VARIANT_Mac_Catalyst='Mac Catalyst'
 
 COMMA=,
@@ -69,59 +67,29 @@ ARCHIVES += $(ARCHIVE_IOS).xcarchive
 ARCHIVES += $(ARCHIVE_IOS_SIMULATOR).xcarchive
 ARCHIVES += $(ARCHIVE_MAC_CATALYST).xcarchive
 
-ARCHIVE_IOS_FRAMEWORK_DIR = $(ARCHIVE_IOS).xcarchive/Products/Library/Frameworks
-ARCHIVE_IOS_SIMULATOR_FRAMEWORK_DIR = $(ARCHIVE_IOS_SIMULATOR).xcarchive/Products/Library/Frameworks
-ARCHIVE_MAC_CATALYST_FRAMEWORK_DIR = $(ARCHIVE_MAC_CATALYST).xcarchive/Products/Library/Frameworks
-
-ARCHIVE_FRAMEWORK_DIRS += $(ARCHIVE_IOS_FRAMEWORK_DIR)
-ARCHIVE_FRAMEWORK_DIRS += $(ARCHIVE_IOS_SIMULATOR_FRAMEWORK_DIR)
-ARCHIVE_FRAMEWORK_DIRS += $(ARCHIVE_MAC_CATALYST_FRAMEWORK_DIR)
-
-ARCHIVE_IOS_FRAMEWORK = $(ARCHIVE_IOS_FRAMEWORK_DIR)/$(FRAMEWORK)
-ARCHIVE_IOS_SIMULATOR_FRAMEWORK = $(ARCHIVE_IOS_SIMULATOR_FRAMEWORK_DIR)/$(FRAMEWORK)
-ARCHIVE_MAC_CATALYST_FRAMEWORK = $(ARCHIVE_MAC_CATALYST_FRAMEWORK_DIR)/$(FRAMEWORK)
-
-ARCHIVE_FRAMEWORKS += $(ARCHIVE_IOS_FRAMEWORK)
-ARCHIVE_FRAMEWORKS += $(ARCHIVE_IOS_SIMULATOR_FRAMEWORK)
-ARCHIVE_FRAMEWORKS += $(ARCHIVE_MAC_CATALYST_FRAMEWORK)
-
 # NOTE: The man page for `xcodebuild` says `archive` "archive[s] a
 # scheme from the build root (SYMROOT)." But it doesn't appear to
-# actually operate on SYMROOT. It doesn't copy the build products and
-# does not seem to need the SYMROOT to be set. In fact, setting the
-# SYMROOT randomly breaks the build.
+# actually operate on SYMROOT. SYMROOT does not seem to be needed. In
+# fact, setting the SYMROOT randomly breaks the build.
 
 $(ARCHIVE_IOS).xcarchive:
 	$(XCODEBUILD) archive -workspace $(WORKSPACE) -scheme $(SCHEME) \
 		-destination generic/platform=iOS \
-		-archivePath $(ARCHIVE_IOS)
+		-config $(CONFIG) -archivePath $(ARCHIVE_IOS)
 
 $(ARCHIVE_IOS_SIMULATOR).xcarchive:
 	$(XCODEBUILD) archive -workspace $(WORKSPACE) -scheme $(SCHEME) \
 		-destination generic/platform='iOS Simulator' \
-		-archivePath $(ARCHIVE_IOS_SIMULATOR)
+		-config $(CONFIG) -archivePath $(ARCHIVE_IOS_SIMULATOR)
 
 
 $(ARCHIVE_MAC_CATALYST).xcarchive:
 	$(XCODEBUILD) archive -workspace $(WORKSPACE) -scheme $(SCHEME) \
 		-destination generic/platform=macOS,variant='Mac Catalyst' \
-		-archivePath $(ARCHIVE_MAC_CATALYST)
+		-config $(CONFIG) -archivePath $(ARCHIVE_MAC_CATALYST)
 
-$(ARCHIVE_FRAMEWORK_DIRS):
-	mkdir -p $@
-
-$(ARCHIVE_IOS_FRAMEWORK):
-	cp -r $(FRAMEWORK_IOS_RELEASE) $(ARCHIVE_IOS_FRAMEWORK_DIR)
-
-$(ARCHIVE_IOS_SIMULATOR_FRAMEWORK):
-	cp -r $(FRAMEWORK_IOS_SIMULATOR_RELEASE) $(ARCHIVE_IOS_SIMULATOR_FRAMEWORK_DIR)
-
-$(ARCHIVE_MAC_CATALYST_FRAMEWORK):
-	cp -r $(FRAMEWORK_MAC_CATALYST_RELEASE) $(ARCHIVE_MAC_CATALYST_FRAMEWORK_DIR)
-
-
-$(ARCHIVES): $(FRAMEWORK_RELEASE_PATHS)
-$(ARCHIVE_FRAMEWORKS): $(ARCHIVES) $(ARCHIVE_FRAMEWORK_DIRS)
+$(ARCHIVES): CONFIG = Release
+$(ARCHIVES): FORCE
 
 
 # XCFrameworks
@@ -145,12 +113,12 @@ ifdef APPLE_DISTRIBUTION_IDENTITY
 xcframework: xcframework-signed
 endif
 
-xcframework-unsigned: $(ARCHIVE_FRAMEWORKS)
+xcframework-unsigned: $(ARCHIVES)
 	-rm -rf $(XCFRAMEWORK_UNSIGNED)
 	$(XCODEBUILD) -create-xcframework $(XCFRAMEWORK_ARGS) \
 		-output $(XCFRAMEWORK_UNSIGNED)
 
-xcframework-signed: $(ARCHIVE_FRAMEWORKS)
+xcframework-signed: $(ARCHIVES)
 	-rm -rf $(XCFRAMEWORK_SIGNED)
 	$(XCODEBUILD) -create-xcframework $(XCFRAMEWORK_ARGS) \
 		-output $(XCFRAMEWORK_SIGNED)
