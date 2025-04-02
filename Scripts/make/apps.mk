5cf0a519089063fc9c7893cb9bc138e37a0fb8bc
Eddie Hillenbrand <haltingproblems0@gmail.com>
2025-04-02 13:40:49 -0500

    Generate Info.plist with non-exempt encryption key for TestFlight

--- Scripts/make/apps.mk
+++ Scripts/make/apps.mk
@@ -11,6 +11,9 @@ UIKIT_XCODEPROJ = Examples/$(UIKIT_PRODUCT_NAME)/$(UIKIT_PRODUCT_NAME)$(APP_VARI
 SWIFTUI_XCCONFIG = Configs/SwiftUIApp-TestFlight.xcconfig
 UIKIT_XCCONFIG = Configs/UIKitApp-TestFlight.xcconfig
 
+SWIFTUI_INFOPLIST = Scripts/templates/SwiftUIApp-TestFlight-Info.plist
+UIKIT_INFOPLIST = Scripts/templates/UIKitApp-TestFlight-Info.plist
+
 SWIFTUI_ARCHIVE = $(ARCHROOT)/$(SWIFTUI_PRODUCT_NAME)$(DEST_iOS_suffix)
 UIKIT_ARCHIVE = $(ARCHROOT)/$(UIKIT_PRODUCT_NAME)$(DEST_iOS_suffix)
 
@@ -22,14 +25,16 @@ $(SWIFTUI_ARCHIVE).xcarchive:
 	$(XCODEBUILD) archive -project $(SWIFTUI_XCODEPROJ) -scheme $(SCHEME) \
 		-destination generic/platform=iOS -config $(CONFIG) \
 		-xcconfig $(SWIFTUI_XCCONFIG) -archivePath $(SWIFTUI_ARCHIVE) \
-		DEVELOPMENT_TEAM=$(APPLE_DEVELOPMENT_TEAM)
+		DEVELOPMENT_TEAM=$(APPLE_DEVELOPMENT_TEAM) \
+		INFOPLIST_FILE=$(SWIFTUI_INFOPLIST)
 
 $(UIKIT_ARCHIVE).xcarchive: SCHEME = $(UIKIT_PRODUCT_NAME)
 $(UIKIT_ARCHIVE).xcarchive:
 	$(XCODEBUILD) archive -project $(UIKIT_XCODEPROJ) -scheme $(SCHEME) \
 		-destination generic/platform=iOS -config $(CONFIG) \
 		-xcconfig $(UIKIT_XCCONFIG) -archivePath $(UIKIT_ARCHIVE) \
-		DEVELOPMENT_TEAM=$(APPLE_DEVELOPMENT_TEAM)
+		DEVELOPMENT_TEAM=$(APPLE_DEVELOPMENT_TEAM) \
+		INFOPLIST_FILE=$(UIKIT_INFOPLIST)
 
 $(APP_ARCHIVES): CONFIG = Release
 
