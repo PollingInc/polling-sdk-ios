e6bfa3441ebd4cdf5b4b40a3a01bd6d7bba5bacb
Eddie Hillenbrand <haltingproblems0@gmail.com>
2025-04-10 17:44:12 -0500

    Write chapter 2 of SwiftUI tutorial

---
+++ Polling/Polling/Polling.docc/SwiftUI/SwiftUICallbackListener/03_ContentView_cb-init.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+import Polling
+
+struct ContentView: View {
+    private var polling: Polling = Polling()
+
+    class Listener: NSObject, PollingDelegate {
+        var view: any View
+        init(view: any View) {
+            self.view = view
+        }
+    }
+
+    init() {
+        polling.customerID = "id_\(Date().timeIntervalSinceReferenceDate)"
+        polling.apiKey = "l4chQVGY1rYlFS7WPmbl5vIqCsZunsuDXUOx"
+    }
+
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Button("Show Survey") {
+                polling.showSurvey("caa4fc7a-a7b9-489c-a7c8-e5e7f8aeeefa")
+            }.padding(.top)
+        }.buttonStyle(.bordered)
+        .padding()
+    }
+}
